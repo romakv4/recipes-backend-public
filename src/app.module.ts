60f079d40feba721bd600a3b59486e574c8d6735
romakv4 <romakv4@gmail.com>
2021-02-23 15:48:51 +0600

    enable text compression

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { RecipesModule } from './recipes/recipes.module';
 import * as dotenv from 'dotenv';
 dotenv.config();
@@ -11,7 +9,7 @@ dotenv.config();
     MongooseModule.forRoot(process.env.MONGO_URI, { useNewUrlParser: true }),
     RecipesModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
